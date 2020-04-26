98a2ffa66b7203032fbb070f0772146d03f1f47e
Valerie Lobas <vallobas@gmail.com>
2020-04-26 17:40:16 -0400

    updated code

--- mission_to_mars/app.py
+++ mission_to_mars/app.py
@@ -1,30 +1,38 @@
-from flask, import Flask, render_template, redirect
+from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
+import pymongo
 import scrape_mars
 
 # Create an instance of our Flask app
 app=Flask(__name__)
 
-# Use flask_pymongo to set up mongo connection
-conn = "mongodb://localhost:27017"
-client = PyMongo.MongoClient(conn)
-db = client.mars_scraperDB
+# Create connection variable
+conn = 'mongodb://localhost:27017'
+
+# Pass connection to the pymongo instance.
+client = pymongo.MongoClient(conn)
+
+# Connect to a database. Will create one if not already available.
+db = client.mars_db
+
+# Drops collection if available to remove duplicates
+db.mars.drop()
 
 @app.route("/")
 def index():
     # Find one record of data from the mongo database
-    mars_db = mongo.db.collection.find_one()
+    mars = db.collection.find_one()
 
     # Return template and data
-    return render_template("index.html", data=mars_db)
+    return render_template("index.html", mars=mars)
 
 @app.route("/scrape")
-def scrape():
+def scraper():
     # Run the scrape function
     mars_data = scrape_mars.scrape()
     
     # Update the Mongo database using update and upsert=True
-    mongo.db.collection.update({}, mars_data, upsert=True)
+    db.collection.update({}, mars_data, upsert=True)
 
     # Redirect back to home page
     return redirect("/", code=302)
--- mission_to_mars/scrape_mars.py
+++ mission_to_mars/scrape_mars.py
@@ -1,6 +1,5 @@
 from bs4 import BeautifulSoup as bs
 import pandas as pd 
-from splinter import Browser
 from selenium import webdriver
 import requests
 import time
@@ -19,9 +18,12 @@ def scrape():
     title = result_first.a.string.strip()
     # Filter results to find article summary
     results2 = soup.find_all('div', class_="rollover_description_inner")
+    # Filter to the latest article summary
+    result2_first = results2[0]
     # Scrape summary of latest article
     p = result2_first.string.strip()
-
+    # Save into dictionary
+    
     # Retrieve JPL Mars page with the requests module
     url2 = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
     response2 = requests.get(url2)
@@ -34,7 +36,7 @@ def scrape():
     url_end = featured_image.get("data-fancybox-href")
     # Create variable to hold whole url
     featured_image_url = f"{url2}{url_end}"
-
+    
     # Retrieve Mars Weather Twitter page with Chromedriver
     url3 = 'https://twitter.com/marswxreport?lang=en'
     driver = webdriver.Chrome()
@@ -42,6 +44,9 @@ def scrape():
     time.sleep(3)
     # Find first tweet
     tweet_div = driver.find_elements_by_xpath('/html/body/div/div/div/div[2]/main/div/div/div/div/div/div/div/div/div[2]/section/div/div/div[1]/div/div/div/article/div/div[2]/div[2]/div[2]/div[1]/div')
+    for x in tweet_div:
+        weather = x.text
+    driver.close()
 
     # Import Mars Facts url with pandas
     url4 = 'https://space-facts.com/mars/'
@@ -52,7 +57,7 @@ def scrape():
     html_table = df.to_html()
     # Strip newlines
     table_stripped = html_table.replace('\n', '')
-
+    
     # Retrieve Mars Hemispheres image urls with 'get requests' module
     url5 = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
     response5 = requests.get(url5)
@@ -72,19 +77,18 @@ def scrape():
         href_split = hemi_href.split("/",3)
         href = href_split[2]
         hemi_url = f"{base_url}{href}{url_suffix}"
-
+    
     # Create dictionary to hold scraped data
-    Mars_Dict:{
+    Mars_Dict={
         "News Title": title,
         "News Summary": p,
         "Featured Image URL": featured_image_url,
-        "Current Mars Weather": tweet_div,
+        "Current Mars Weather": weather,
         "Mars Facts Table": table_stripped,
         "Hemisphere Image URL": hemi_url,
         "Hemisphere Name": hemi
     }
-
-    browser.quit()
     
+    print(Mars_Dict)
     return Mars_Dict
     
